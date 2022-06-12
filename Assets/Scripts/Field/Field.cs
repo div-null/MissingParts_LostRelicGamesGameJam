723f7e0fbf5a945fb4b94cdbd05ea4fd2d5690ca
fivan <46835435+f1van0@users.noreply.github.com>
2022-06-12 18:07:51 +0700

    create cells on field prototype and prefabs for cells

--- Assets/Scripts/Field/Field.cs
+++ Assets/Scripts/Field/Field.cs
@@ -6,7 +6,10 @@ using UnityEngine;
 public class Field : MonoBehaviour
 {
     public static Field Instance;
-    
+    public GameObject WallCellPrefab;
+    public GameObject EmptyCellPrefab;
+    public GameObject HoleCellPrefab;
+    public GameObject CharacterPartPrefab;
     public Cell[,] Cells;
     private List<CharacterPart> CharacterParts;
 
@@ -20,7 +23,30 @@ public class Field : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Cells = new Cell[5, 5];
+        CharacterParts = new List<CharacterPart>();
         
+        for (int j = 0; j < 5; j++)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Cell newCell;
+                if (i == 0 || i == 4 || j == 0 || j == 4)
+                    newCell = Instantiate(WallCellPrefab, new Vector3(i, j, -1), Quaternion.identity).GetComponent<Cell>();
+                else
+                    newCell = Instantiate(EmptyCellPrefab, new Vector3(i, j, -1), Quaternion.identity).GetComponent<Cell>();
+                
+                newCell.transform.SetParent(transform);
+                Cells[i, j] = newCell;
+                
+                if (i == 3 && j == 2)
+                {
+                    CharacterPart newCharacterPart = Instantiate(CharacterPartPrefab, new Vector3(i, j, -2), Quaternion.identity).GetComponent<CharacterPart>();
+                    CharacterParts.Add(newCharacterPart);
+                    newCharacterPart.transform.SetParent(transform);
+                }
+            }
+        }
     }
 
     // Update is called once per frame
