52e44bf0acb22f35ec232a2a81eb817351a1ba31
Optifun <frdima19@gmail.com>
2022-06-12 14:02:42 +0700

    Add LevelContainer

---
+++ Assets/Scripts/Infrastructure/Scope/LevelContainer.cs
@@ -0,0 +1,13 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Infrastructure.Scope
+{
+    public class LevelContainer : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            base.Configure(builder);
+        }
+    }
+}
