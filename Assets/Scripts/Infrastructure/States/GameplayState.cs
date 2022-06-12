4ee202ec18a5f96763a80691784358fa47b5d580
fivan <46835435+f1van0@users.noreply.github.com>
2022-06-12 19:59:31 +0700

    Add Game scene

---
+++ Assets/Scripts/Infrastructure/States/GameplayState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Infrastructure.States
+{
+    public class GameplayState : IState
+    {
+        private SceneLoader _loader;
+
+        public GameplayState(SceneLoader loader)
+        {
+            _loader = loader;
+        }
+
+        public void Enter()
+        {
+            Debug.Log("Entered Gameplay state");
+            _loader.Load("Game");
+        }
+
+        public void Exit()
+        {
+            Debug.Log("Exited Gameplay state");
+        }
+    }
+}
