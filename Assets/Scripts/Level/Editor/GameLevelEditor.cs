20a653ff00f63de7b3460738ccbc19e090c70576
Optifun <frdima19@gmail.com>
2022-06-13 15:31:35 +0700

    Add unready LevelEditor

---
+++ Assets/Scripts/Level/Editor/GameLevelEditor.cs
@@ -0,0 +1,42 @@
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace Level.Editor
+{
+    // [CustomEditor(typeof(GameLevel))]
+    // public class GameLevelEditor : UnityEditor.Editor
+    // {
+    //     public override VisualElement CreateInspectorGUI()
+    //     {
+    //         VisualElement inspector = new VisualElement();
+    //
+    //         inspector.Add(new Label("This is a custom inspector"));
+    //
+    //         var finishEnum = new EnumField("Finish Color", CellColor.Green);
+    //         inspector.Add(finishEnum);
+    //
+    //         VisualTreeAsset visualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Level/Editor/Level_editor.uxml");
+    //         VisualTreeAsset cellEditor = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Scripts/Level/Editor/cell_editor.uxml");
+    //
+    //         visualTree.CloneTree(inspector);
+    //         VisualElement mapContainer = inspector.Q("mapEditor");
+    //
+    //         var level = serializedObject.targetObject as GameLevel;
+    //         if (level == null)
+    //             return inspector;
+    //
+    //         for (int j = 0; j < level.MapHeight; j++)
+    //         {
+    //             for (int i = 0; i < level.MapWidth; i++)
+    //             {
+    //                 var editor = new CellEditor();
+    //                 editor.SetCell(level.Cells[i][j]);
+    //                 mapContainer.Add(editor);
+    //             }
+    //         }
+    //
+    //         return inspector;
+    //     }
+    // }
+}
--- Assets/Scripts/Level/GameLevel.cs
+++ Assets/Scripts/Level/GameLevel.cs
@@ -7,10 +7,23 @@ namespace Level
     [Serializable]
     public class GameLevel : ScriptableObject
     {
-        public CellType[,] Cells;
+        public CellType[][] Cells;
         public int MapHeight = 2;
         public int MapWidth = 2;
         public CellColor FinishColor;
         public PlayerPart[] PlayerParts;
+
+        private void Reset()
+        {
+            Cells = new CellType[MapHeight][];
+            for (int j = 0; j < Cells.GetLength(1); j++)
+            {
+                Cells[j] = new CellType[MapWidth];
+                for (int i = 0; i < Cells.GetLength(0); i++)
+                {
+                    Cells[j][i] = CellType.Floor;
+                }
+            }
+        }
     }
 }
