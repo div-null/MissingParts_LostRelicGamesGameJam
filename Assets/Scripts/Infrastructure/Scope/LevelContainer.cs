374cd7dd1f7154ce60189946164e9b3647b79106
Optifun <frdima19@gmail.com>
2022-06-13 19:54:18 +0700

    Remove CharacterFactory

--- Assets/Scripts/Infrastructure/Scope/LevelContainer.cs
+++ Assets/Scripts/Infrastructure/Scope/LevelContainer.cs
@@ -1,4 +1,5 @@
 using Game;
+using LevelEditor;
 using VContainer;
 using VContainer.Unity;
 
@@ -8,27 +9,8 @@ namespace Infrastructure.Scope
     {
         protected override void Configure(IContainerBuilder builder)
         {
-            builder.Register<CharacterFactory>(Lifetime.Scoped);
             builder.Register<LevelFactory>(Lifetime.Scoped);
             builder.RegisterEntryPoint<LevelEntryPoint>();
-
-            builder.Register(container =>
-            {
-                var levelEntryPoint = container.Resolve<LevelFactory>();
-                return levelEntryPoint.Create(8, 8);
-            }, Lifetime.Scoped);
-
-            builder.Register(container =>
-            {
-                var characterFactory = container.Resolve<CharacterFactory>();
-                return characterFactory.Create();
-            }, Lifetime.Scoped);
-
-            // builder.RegisterEntryPointExceptionHandler(exception =>
-            // {
-            //     Debug.Log(exception);
-            //     Debug.Log(exception.StackTrace);
-            // });
         }
     }
 }
--- Assets/Scripts/Infrastructure/Scope/LevelEntryPoint.cs
+++ Assets/Scripts/Infrastructure/Scope/LevelEntryPoint.cs
@@ -1,20 +1,36 @@
-﻿using VContainer.Unity;
+﻿using Game;
+using LevelEditor;
+using VContainer.Unity;
 
 namespace Infrastructure.Scope
 {
     public class LevelEntryPoint : IStartable
     {
-        private Field _field;
-        private Character _character;
+        private LevelFactory _factory;
+        private int _level;
+        private LevelLoader _levelLoader;
 
-        public LevelEntryPoint(Field field, Character character)
+        public LevelEntryPoint(LevelFactory factory, LevelLoader levelLoader)
         {
-            _character = character;
-            _field = field;
+            _levelLoader = levelLoader;
+            _factory = factory;
+            _level = 1;
         }
 
         public void Start()
         {
+        }
+
+        public void LoadNextLevel()
+        {
+            GameLevel level = _level switch
+            {
+                1 => _levelLoader.LoadLevel(LevelLoader.Level.Lvl1),
+                2 => _levelLoader.LoadLevel(LevelLoader.Level.Lvl2),
+                3 => _levelLoader.LoadLevel(LevelLoader.Level.Lvl3),
+            };
+            Field field = _factory.CreateField(level);
+            Character character = _factory.CreateCharacter(level, field);
         }
     }
 }
