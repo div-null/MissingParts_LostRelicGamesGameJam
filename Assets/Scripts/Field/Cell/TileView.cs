ba6e769edde69dbc92d92ca99458ec839b12ab56
Optifun <frdima19@gmail.com>
2022-06-17 18:51:17 +0700

    Add updated pit cell

---
+++ Assets/Scripts/Field/Cell/TileView.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TileView : MonoBehaviour
+{
+    public SpriteRenderer First;
+    public SpriteRenderer Second;
+    public SpriteRenderer Third;
+    public SpriteRenderer Fourth;
+
+    public Sprite OuterCorner;
+    public Sprite InnerCorner;
+    public Sprite Vertical;
+
+}
