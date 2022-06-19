c3de6c7d15c1bbfaf8180e9fb2741e14a3d25226
Optifun <frdima19@gmail.com>
2022-06-19 14:41:16 +0700

    Add OpenHyperLinks script

---
+++ Assets/Scripts/UI/OpenHyperLinks.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace UI
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class OpenHyperlinks : MonoBehaviour, IPointerClickHandler {
+        [SerializeField]
+        private TMP_Text TMP_text;
+
+        public void OnPointerClick(PointerEventData eventData) {
+            int linkIndex = TMP_TextUtilities.FindIntersectingLink(TMP_text, Input.mousePosition, Camera.current);
+            if( linkIndex != -1 ) { // was a link clicked?
+                TMP_LinkInfo linkInfo = TMP_text.textInfo.linkInfo[linkIndex];
+
+                // open the link id as a url, which is the metadata we added in the text field
+                Application.OpenURL(linkInfo.GetLinkID());
+            }
+        }
+    }
+}
