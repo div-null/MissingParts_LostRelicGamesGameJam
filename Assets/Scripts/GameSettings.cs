4ffeb126481f89a5cfd24477069b773f19da326c
Optifun <frdima19@gmail.com>
2022-08-08 19:38:10 +0700

    Move SO to Scripts folder

--- Assets/Scripts/GameSettings.cs
+++ Assets/Scripts/GameSettings.cs
@@ -1,22 +1,16 @@
-using Game;
 using Game.Cell;
 using Game.Character;
 using Infrastructure.Scope;
 using UnityEngine;
-using CharacterController = Game.Character.CharacterController;
 
 [CreateAssetMenu(fileName = "GameSettings", menuName = "Create settings", order = 0)]
 public class GameSettings : ScriptableObject
 {
-    // public PitCellPrefab PitCellPrefabs;
-    // public WallCellPrefab WallCellPrefabs;
     public Cell WallCellPrefab;
     public Cell EmptyCellPrefab;
     public Cell PitCellPrefab;
     public Cell FinishCellPrefab;
     public CharacterPartContainer CharacterPartPrefab;
-    public Field FieldPrefab;
-    public CharacterController CharacterPrefab;
     public GameLifetimeScope GameLifetimeScope;
     public GameContainer LevelContainer;
     public MenuLifetime MenuLifetime;
