7091a35c54633526b9f2634f5cbb4d85226269b8
Optifun <frdima19@gmail.com>
2022-08-08 16:03:32 +0700

    Update Cell: add OnDrawGizmos

--- Assets/Scripts/Field/Cell/Cell.cs
+++ Assets/Scripts/Field/Cell/Cell.cs
@@ -5,36 +5,19 @@ namespace Assets.Scripts.Field.Cell
 {
     public class Cell : MonoBehaviour
     {
+        public const float CellSize = 1f;
+        
         public Vector2Int Position;
         public CellType CellType;
         public CharacterPartContainer? Container;
 
         [field: SerializeField] public DirectionType BorderDirections { get; private set; }
-        public const float CellSize = 1f;
 
         public void Initialize(Vector2Int position, CellType cellType, DirectionType borderDirections)
         {
             BorderDirections = borderDirections;
             Position = position;
             CellType = cellType;
-
-            Vector3 leftSide = CellSize * 0.5f * Vector3.left;
-            Vector3 rightSide = CellSize * 0.5f * Vector3.right;
-            Vector3 topSide = CellSize * 0.5f * Vector3.up;
-            Vector3 bottomSide = CellSize * 0.5f * Vector3.down;
-
-            Debug.DrawRay(Vector3.zero, Vector3.up);
-            Debug.DrawRay(Vector3.zero, Vector3.right);
-
-            var center = new Vector3(Position.x, Position.y, -1);
-            if (BorderDirections.HasFlag(DirectionType.Up))
-                Debug.DrawLine(leftSide + topSide + center, rightSide + topSide + center, Color.blue, Single.PositiveInfinity);
-            if (BorderDirections.HasFlag(DirectionType.Down))
-                Debug.DrawLine(leftSide + bottomSide + center, rightSide + bottomSide + center, Color.blue, Single.PositiveInfinity);
-            if (BorderDirections.HasFlag(DirectionType.Left))
-                Debug.DrawLine(leftSide + topSide + center, leftSide + bottomSide + center, Color.blue, Single.PositiveInfinity);
-            if (BorderDirections.HasFlag(DirectionType.Right))
-                Debug.DrawLine(rightSide + topSide + center, rightSide + bottomSide + center, Color.blue, Single.PositiveInfinity);
         }
 
         public bool IsWall() =>
@@ -67,6 +50,24 @@ namespace Assets.Scripts.Field.Cell
         public void AssignCharacterPart(CharacterPartContainer characterPartContainer)
         {
             Container = characterPartContainer;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Vector3 leftSide =   CellSize * 0.5f * Vector3.left;
+            Vector3 rightSide =  CellSize * 0.5f * Vector3.right;
+            Vector3 topSide =    CellSize * 0.5f * Vector3.up;
+            Vector3 bottomSide = CellSize * 0.5f * Vector3.down;
+
+            var center = new Vector3(Position.x, Position.y, 0);
+            if (BorderDirections.HasFlag(DirectionType.Up))
+                Debug.DrawLine(center + leftSide + topSide,    center + rightSide + topSide,    Color.blue);
+            if (BorderDirections.HasFlag(DirectionType.Down))
+                Debug.DrawLine(center + leftSide + bottomSide, center + rightSide + bottomSide, Color.blue);
+            if (BorderDirections.HasFlag(DirectionType.Left))
+                Debug.DrawLine(center + leftSide + topSide,    center + leftSide + bottomSide,  Color.blue);
+            if (BorderDirections.HasFlag(DirectionType.Right))
+                Debug.DrawLine(center + rightSide + topSide,   center + rightSide + bottomSide, Color.blue);
         }
     }
 }
